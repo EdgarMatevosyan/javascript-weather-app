dcfd66fde08c99e02ab8df75b39c204938120718
Edgar Matevosyan <edgarmatevosyan@gmail.com>
2021-04-12 18:40:35 -0700

    classes added, ui updated

--- app.js
+++ app.js
@@ -1,7 +1,20 @@
 // Init weather object
 const weather = new Weather("glendale", "CA");
 
-weather
-  .getWeather()
-  .then((results) => console.log(results))
-  .catch((err) => console.log(err));
+// Init UI
+const ui = new UI();
+
+// Gey weather on DOM load
+document.addEventListener("DOMContentLoaded", getWeather());
+
+// weather.changeLocation("glendale", "CA");
+
+function getWeather() {
+  weather
+    .getWeather()
+    .then((results) => {
+      console.log(results);
+      ui.paint(results);
+    })
+    .catch((err) => console.log(err));
+}
